52a4e2e416e83d92d97ba18f16c29ae03ee56b6f
AIcrazyseal <18622003295@163.com>
2026-02-06 16:53:09 +0800

    代码测试

---
+++ Code/app.js
@@ -0,0 +1,5 @@
+function say(name) {
+console.log(`hello world${name}`)
+console.log("hello vscode")
+}
+say("AI")
---
+++ Code/test .py
@@ -0,0 +1,22 @@
+"""
+import random
+a = random.randint(0,100)
+b = random.randint(0,100)
+print("a=",a,"b=",b,"a*b=",a*b)
+"""
+
+import os
+import sys
+# 查看当前工作目录
+print("当前工作目录:", os.getcwd())
+# 列出当前目录的文件
+print("当前目录的文件:", os.listdir('.'))
+
+# 检查文件是否存在
+if os.path.exists('a.txt'):
+    print("文件存在")
+else:
+    print("文件不存在")
+f = open('code/a.txt','r',encoding='utf-8')
+s = f.readline()
+print(s)
